012d0beb2f4d772e9d7e6a06c9fd68a0cfb4108b
muzonchiki <muzonchiki@yandex.ru>
2017-12-07 14:39:26 +0300

    first commit

--- test/index.js
+++ test/index.js
@@ -1,176 +1,111 @@
-import { assert } from 'chai';
-import { randomNumberArray, randomStringArray, randomValue as random } from '../helper';
-import { calculator, isAllTrue, isSomeTrue, returnBadArguments } from '../src/index';
+import assert from 'assert';
+import {
+    addListener,
+    removeListener,
+    skipDefault,
+    emulateClick,
+    delegate,
+    once
+} from '../src/index';
 
-describe('ДЗ 2 - работа с исключениями и отладчиком', () => {
-    describe('isAllTrue', () => {
-        it('должна вызывать fn для всех элементов массива', () => {
-            let array = random('array', 1);
-            let pass = [];
+describe('ДЗ 5.1 - DOM Events', () => {
+    describe('addListener', () => {
+        it('должна добавлять обработчик событий элемента', () => {
+            let target = document.createElement('div');
+            let eventName = 'click';
+            let passed = false;
+            let fn = () => passed = true;
 
-            isAllTrue(array, e => pass.push(e));
+            addListener(eventName, target, fn);
 
-            assert.deepEqual(pass, array);
-        });
-
-        it('должна вернуть true, если fn вернула true для всех элементов массива', () => {
-            let array = randomNumberArray();
-            let result = isAllTrue(array, Number.isFinite);
-
-            assert.isTrue(result);
-        });
-
-        it('должна вернуть false, если fn вернула false хотя бы для одного элемента массива', () => {
-            let array = randomNumberArray();
-
-            array.push(random('string'));
-            let result = isAllTrue(array, Number.isFinite);
-
-            assert.isFalse(result);
-        });
-
-        it('должна выбросить исключение, если передан пустой массив', () => {
-            assert.throws(isAllTrue.bind(null, [], () => {
-            }), 'empty array');
-        });
-
-        it('должна выбросить исключение, если передан не массив', () => {
-            assert.throws(isAllTrue.bind(null,':(', () => {
-            }), 'empty array');
-            assert.throws(isAllTrue.bind(null, {}), 'empty array');
-        });
-
-        it('должна выбросить исключение, если fn не функция', () => {
-            let array = randomNumberArray();
-
-            assert.throws(isAllTrue.bind(null, array, ':('), 'fn is not a function');
+            assert(!passed);
+            target.dispatchEvent(new CustomEvent(eventName));
+            assert(passed);
         });
     });
 
-    describe('isSomeTrue', () => {
-        it('должна вернуть true, если fn вернула true хотя бы для одного элемента массива', () => {
-            let array = randomStringArray().concat(random('number'));
-            let result = isSomeTrue(array, Number.isFinite);
+    describe('removeListener', () => {
+        it('должна удалять обработчик событий элемента', () => {
+            let target = document.createElement('div');
+            let eventName = 'click';
+            let passed = false;
+            let fn = () => passed = true;
 
-            assert.isTrue(result);
-        });
+            target.addEventListener(eventName, fn);
 
-        it('должна вернуть false, если fn не вернула true хотя бы для одного элемента массива', () => {
-            let array = randomStringArray();
-            let result = isSomeTrue(array, Number.isFinite);
+            removeListener(eventName, target, fn);
 
-            assert.isFalse(result);
-        });
-
-        it('должна выбросить исключение, если передан пустой массив', () => {
-            assert.throws(isSomeTrue.bind(null, [], () => {
-            }), 'empty array');
-        });
-
-        it('должна выбросить исключение, если передан не массив', () => {
-            assert.throws(isSomeTrue.bind(null, ':(', () => {
-            }), 'empty array');
-            assert.throws(isSomeTrue.bind(null, {}), 'empty array');
-        });
-
-        it('должна выбросить исключение, если fn не функция', () => {
-            let array = randomNumberArray();
-
-            assert.throws(isSomeTrue.bind(null, array, ':('), 'fn is not a function');
+            target.dispatchEvent(new CustomEvent(eventName));
+            assert(!passed);
         });
     });
 
-    describe('returnBadArguments', () => {
-        it('должна вызывать fn для всех элементов массива', () => {
-            let array = random('array', 1);
-            let pass = [];
+    describe('skipDefault', () => {
+        it('должна добавлять такой обработчик, который предотвращает действие по умолчанию', () => {
+            let target = document.createElement('div');
+            let eventName = 'click';
+            let result;
 
-            returnBadArguments(e => pass.push(e), ...array);
+            skipDefault(eventName, target);
 
-            assert.deepEqual(pass, array);
-        });
-
-        it('должна вернуть массив с аргументами, для которых fn выбрасила исключение', () => {
-            let evenNumbers = randomNumberArray('even');
-            let oddNumbers = randomNumberArray('odd');
-            let fn = a => {
-                if (a % 2 != 0) {
-                    throw new Error('not even');
-                }
-            };
-            let result = returnBadArguments(fn, ...evenNumbers, ...oddNumbers);
-
-            assert.deepEqual(result, oddNumbers);
-        });
-
-        it('должна вернуть массив пустой массив, если не передано дополнительных аргументов', () => {
-            let fn = () => ':)';
-            let result = returnBadArguments(fn);
-
-            assert.deepEqual(result, []);
-        });
-
-        it('должна выбросить исключение, если fn не функция', () => {
-            assert.throws(returnBadArguments.bind(null, ':('), 'fn is not a function');
+            result = target.dispatchEvent(new CustomEvent(eventName, { cancelable: true }));
+            assert(!result);
         });
     });
 
-    describe('calculator', () => {
-        it('должна возвращать объект с методами', () => {
-            let calc = calculator();
+    describe('emulateClick', () => {
+        it('должна эмулировать клик по элементу', () => {
+            let target = document.createElement('div');
+            let eventName = 'click';
+            let passed = false;
+            let fn = () => passed = true;
 
-            assert.includeMembers(Object.keys(calc), ['sum', 'dif', 'div', 'mul']);
+            target.addEventListener(eventName, fn);
+
+            emulateClick(target);
+
+            assert(passed);
         });
+    });
 
-        it('метод sum должен складывать аргументы', () => {
-            let initialValue = random('number');
-            let calc = calculator(initialValue);
-            let args = randomNumberArray();
+    describe('delegate', () => {
+        it('должна добавлять обработчик кликов, который реагирует только на клики по кнопкам', () => {
+            let target = document.createElement('div');
+            let eventName = 'click';
+            let passed = false;
+            let fn = () => passed = true;
 
-            assert.strictEqual(calc.sum(...args), args.reduce((prev, current) => prev + current, initialValue));
+            target.innerHTML = '<div></div><a href="#"></a><p></p><button></button>';
+
+            delegate(target, fn);
+
+            assert(!passed);
+            target.children[0].dispatchEvent(new CustomEvent(eventName, { bubbles: true }));
+            assert(!passed);
+            target.children[1].dispatchEvent(new CustomEvent(eventName, { bubbles: true }));
+            assert(!passed);
+            target.children[2].dispatchEvent(new CustomEvent(eventName, { bubbles: true }));
+            assert(!passed);
+            target.children[3].dispatchEvent(new CustomEvent(eventName, { bubbles: true }));
+            assert(passed);
         });
+    });
 
-        it('метод dif должен вычитать аргументы', () => {
-            let initialValue = random('number');
-            let calc = calculator(initialValue);
-            let args = randomNumberArray();
+    describe('once', () => {
+        it('должна добавлять обработчик кликов, который сработает только один раз и удалится', () => {
+            let target = document.createElement('div');
+            let eventName = 'click';
+            let passed = 0;
+            let fn = () => passed++;
 
-            assert.strictEqual(calc.dif(...args), args.reduce((prev, current) => prev - current, initialValue));
-        });
+            once(target, fn);
 
-        it('метод div должен делить аргументы', () => {
-            let initialValue = random('number');
-            let calc = calculator(initialValue);
-            let args = randomNumberArray();
-
-            assert.strictEqual(calc.div(...args), args.reduce((prev, current) => prev / current, initialValue));
-        });
-
-        it('метод div должен выбрасывать исключение, если хотя бы один из аргументов равен 0', () => {
-            let initialValue = random('number');
-            let calc = calculator(initialValue);
-            let args = [...randomNumberArray(), 0];
-
-            assert.throws(calc.div.bind(null, ...args), 'division by 0');
-        });
-
-        it('метод mul должен умножать аргументы', () => {
-            let initialValue = random('number');
-            let calc = calculator(initialValue);
-            let args = randomNumberArray();
-
-            assert.strictEqual(calc.mul(...args), args.reduce((prev, current) => prev * current, initialValue));
-        });
-
-        it('функция должна выбрасывать исключение, если number не является числом', () => {
-            assert.throws(calculator.bind(null, ':('), 'number is not a number');
-        });
-
-        it('значение по умолчанию для аргумента number должно быть равно 0', () => {
-            let calc = calculator();
-            let args = randomNumberArray();
-
-            assert.strictEqual(calc.sum(...args), args.reduce((prev, current) => prev + current));
+            assert.equal(passed, 0);
+            target.dispatchEvent(new CustomEvent(eventName));
+            assert.equal(passed, 1);
+            target.dispatchEvent(new CustomEvent(eventName));
+            assert.equal(passed, 1);
+            target.dispatchEvent(new CustomEvent(eventName));
         });
     });
 });
